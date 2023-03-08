f348dbd5d5a02dc527cfe9dc3ced27dc213a0ad9
conny <jmg41490400@gmail.com>
2023-03-08 17:52:43 +0900

    counsel 레포지토리 정의

---
+++ src/main/java/com/conny/loan/repository/CounselRepository.java
@@ -0,0 +1,10 @@
+package com.conny.loan.repository;
+
+import com.conny.loan.domain.Counsel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CounselRepository extends JpaRepository<Counsel, Long> {
+
+}
